6ab1034d6cf41054a99e4db492a3f5214d1c4318
sarampau <mannysth@yahoo.com>
2018-08-09 17:45:11 -0700

    added MLAB resource

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my-blog');
 mongoose.Promise = Promise;
 
 const app = express();
